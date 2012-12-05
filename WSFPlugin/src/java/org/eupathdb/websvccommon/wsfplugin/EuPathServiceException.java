5706a75a8f7c747311b8138863a54952484b1389
Xin Gao <xingao@upenn.edu>
2012-12-05 00:36:58 +0000

    add eclipse project; add generic method to access connection.
    git-svn-id: https://cbilsvn.pmacs.upenn.edu/svn/apidb/EuPathWebSvcCommon/trunk@52002 735e2a04-f8fc-0310-8a1b-f2942603c481

---
+++ WSFPlugin/src/java/org/eupathdb/websvccommon/wsfplugin/EuPathServiceException.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package org.eupathdb.websvccommon.wsfplugin;
+
+/**
+ * @author jerric
+ * 
+ */
+public class EuPathServiceException extends Exception {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 4394578587877062402L;
+
+  /**
+   * @param message
+   */
+  public EuPathServiceException(String message) {
+    super(message);
+  }
+
+  /**
+   * @param cause
+   */
+  public EuPathServiceException(Throwable cause) {
+    super(cause);
+  }
+
+  /**
+   * @param message
+   * @param cause
+   */
+  public EuPathServiceException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  /**
+   * @param message
+   * @param cause
+   * @param enableSuppression
+   * @param writableStackTrace
+   */
+  public EuPathServiceException(String message, Throwable cause,
+      boolean enableSuppression, boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
+}
