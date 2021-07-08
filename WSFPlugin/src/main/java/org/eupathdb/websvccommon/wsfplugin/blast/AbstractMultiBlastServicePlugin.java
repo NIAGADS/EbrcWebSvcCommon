ca2f128bb9adde631747e394d2c6b8c0a858dcc7
Jamie Long <jtlong3rd@gmail.com>
2021-07-08 15:08:59 -0400

    Fix bug where "subjobs" were appearing in the client's "All Jobs" table

--- WSFPlugin/src/main/java/org/eupathdb/websvccommon/wsfplugin/blast/AbstractMultiBlastServicePlugin.java
+++ WSFPlugin/src/main/java/org/eupathdb/websvccommon/wsfplugin/blast/AbstractMultiBlastServicePlugin.java
@@ -116,6 +116,7 @@ public abstract class AbstractMultiBlastServicePlugin extends AbstractPlugin {
     JSONObject newJobRequestJson = new JSONObject()
       .put("site", projectId)
       .put("maxSequences", 1)
+      .put("isPrimary", false)
       .put("config", MultiBlastServiceParams.buildNewJobRequestConfigJson(request.getParams()))
       .put("targets", MultiBlastServiceParams.buildNewJobRequestTargetJson(request.getParams()));
 
